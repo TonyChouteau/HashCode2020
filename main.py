e2fe43e857438fdf65378ebdf0546ffce6dc066d
TonyChouteau <tony.chouteau@outlook.fr>
2020-02-20 19:47:29 +0100

    not working

--- main.py
+++ main.py
@@ -8,8 +8,24 @@ def readFile():
     f= open("a_example.txt","r")
     if f.mode == 'r':
         content = f.read()
-        #print(contents.split("\n")[1].split(" "))
-        books.append(l.Library)
+
+        #List of scores of books
+        scores = []
+        for i in range(int(contents.split("\n")[0].split(" ")[0])):
+            scores.append(int(contents.split("\n")[0].split(" ")[i]))
+
+        #Creating libs
+        for i in range(int(contents.split("\n")[0].split(" ")[1])):
+            #Creating the library from the file
+            newLib = l.Library(i,int(contents.split("\n")[i*2+2].split(" ")[1], int(contents.split("\n")[i*2+2].split(" ")[2]))
+            
+            books = []
+            #Creating books
+            for j in range(int(contents.split("\n")[i*2+2].split(" ")[0])):
+                books.append(l.Book(int(contents.split("\n")[i*2+3].split(" ")[j]), )
+
+            #Creating libs to the list
+            libs.append()
 
         maxi = int(content.split("\n")[0].split(" ")[0])
         contenList = content.split("\n")[1].split(" ")
