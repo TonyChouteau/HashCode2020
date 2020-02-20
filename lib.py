510bc4f6bc34c3767cd5b442f5774f7c4abd922c
Daniel Zhu <dzhu@enssat.fr>
2020-02-20 19:45:03 +0100

    Affichage des valeurs des objets (__str__()).

--- lib.py
+++ lib.py
@@ -16,6 +16,16 @@ class Book:
         self.id    = ID_book
         self.score = score
 
+    def __str__( self ):
+        """
+        Affichage du livre courant
+
+        :return: chaine de caractère contenant les attributs de l'objet courant
+        """
+
+        return f"Livre<id: {self.id}, score: {self.score}>"
+
+
 
 # ---------------------------------------------------------------------------------------------------------------------
 # Classe des bibliothèques
@@ -36,3 +46,12 @@ class Library:
         self.books = []
         self.time  = time
         self.scannable_books_per_day = scannable_books_per_day
+
+    def __str__( self ):
+        """
+        Affichage de la bibliothèque courante
+
+        :return: chaine de caractère contenant les attributs de l'objet courant
+        """
+
+        return f"Bibliothèque<id: {self.id}, time: {self.time}, scannable_books_per_day: {self.scannable_books_per_day}, books: {self.books} >"
