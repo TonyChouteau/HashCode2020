750473a5fcbd0b125f3360f9c05743715757fe62
TonyChouteau <tony.chouteau@outlook.fr>
2020-02-20 18:37:58 +0100

    added practice work ; may help

---
+++ Practice/main.py
@@ -0,0 +1,62 @@
+#===================================
+# Variables
+
+maxi = 0
+array = []
+
+res = []
+summ = 0
+
+#===================================
+# Functions
+
+def readFile():
+    global maxi
+
+    f= open("b_small.in","r")
+    if f.mode == 'r':
+        content = f.read()
+        #print(contents.split("\n")[1].split(" "))
+        maxi = int(content.split("\n")[0].split(" ")[0])
+        contenList = content.split("\n")[1].split(" ")
+        for c in contenList:
+            array.append(int(c))
+    
+    f.close()
+
+    if f.mode == 'r':
+        return 1
+    return 0
+
+##
+
+def choose():
+    global maxi
+    global summ
+    global res
+
+    currentSum = 0
+    currentRes = []
+
+    print(len(array), maxi)
+    i = len(array)-1
+    while (i>=0 and summ != maxi):
+        if array[i]<(maxi-currentSum):
+            currentRes.append(i)
+            currentSum+=array[i]
+            if (currentSum > summ):
+                summ = currentSum
+                res = list(currentRes)
+        i-=1
+        print(summ, res)
+        
+
+#===================================
+# Entry Point
+
+def main():    
+    if (readFile()):
+        choose()
+ 
+if __name__ == '__main__':
+    main()
