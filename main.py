18469e451d5342b90d71bb444d7c68db660384e7
Thomas Lepercq <tlepercq@enssat.fr>
2020-02-20 18:59:53 +0100

    Read File Func

--- main.py
+++ main.py
@@ -2,3 +2,22 @@ import numpy as np
 
 #Books
 #Copybooks = Books.sort().reverse()
+books = []
+
+def readFile():
+    global maxi
+
+    f= open("b_small.in","r")
+    if f.mode == 'r':
+        content = f.read()
+        #print(contents.split("\n")[1].split(" "))
+        maxi = int(content.split("\n")[0].split(" ")[0])
+        contenList = content.split("\n")[1].split(" ")
+        for c in contenList:
+            books.append(int(c))
+    
+    f.close()
+
+    if f.mode == 'r':
+        return 1
+    return 0
